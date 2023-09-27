cf91931af86e8296b5be68648e452658a788ac94
JakubReha <kubareha107@gmail.com>
2023-09-27 12:55:19 +0200

    aggregated ground truth

--- darpa_labelling/tools/aggregate.py
+++ darpa_labelling/tools/aggregate.py
@@ -5,11 +5,11 @@ import argparse
 def aggregate(ground_truth_folder, edges_path, dataset):
     edges = pd.read_csv(edges_path)
     if dataset == "trace" or dataset == "theia":
-        firefox_backdoor = pd.read_csv(os.path.join(ground_truth_folder, f"TC3_{dataset}_firefox_backdoor_final.csv"))
-        browser_extension = pd.read_csv(os.path.join(ground_truth_folder, f"TC3_{dataset}_browser_extension_final.csv"))
+        firefox_backdoor = pd.read_csv(os.path.join(ground_truth_folder, f"TC3_{dataset}_firefox_backdoor.csv"))
+        browser_extension = pd.read_csv(os.path.join(ground_truth_folder, f"TC3_{dataset}_browser_extension.csv"))
         if dataset == "trace":
-            pine_phishing_exe = pd.read_csv(os.path.join(ground_truth_folder, f"TC3_{dataset}_pine_phishing_exe_final.csv"))
-            trace_thunderbird_phishing_exe = pd.read_csv(os.path.join(ground_truth_folder, f"TC3_{dataset}_thunderbird_phishing_exe_final.csv"))
+            pine_phishing_exe = pd.read_csv(os.path.join(ground_truth_folder, f"TC3_{dataset}_pine_phishing_exe.csv"))
+            trace_thunderbird_phishing_exe = pd.read_csv(os.path.join(ground_truth_folder, f"TC3_{dataset}_thunderbird_phishing_exe.csv"))
             pine_phishing_exe = pine_phishing_exe[pine_phishing_exe.edge_hash_id.isin(edges.hash_id)]
             trace_thunderbird_phishing_exe = trace_thunderbird_phishing_exe[trace_thunderbird_phishing_exe.edge_hash_id.isin(edges.hash_id)]
             pine_phishing_exe.to_csv(os.path.join(ground_truth_folder, f"TC3_{dataset}_pine_phishing_exe_final_aggregated.csv"))
@@ -29,3 +29,5 @@ if __name__=="__main__":
     parser.add_argument("--edges_path", required=True)
     parser.add_argument("--dataset", type = str.lower, choices=['trace', 'theia'])
     args = parser.parse_args()
+    print(args)
+    aggregate(args.ground_truth_folder, args.edges_path, args.dataset)
